ff2dad1b4db6095f53775c671b77b72cfe718d8d
Ariel Cordes <corde171@morris.umn.edu>
2019-04-16 16:55:45 -0500

    Add basic alphabet, dictionary, and other functionality to MagSysGen.java with help from Interpreter.java

--- src/main/java/Interpreter.java
+++ src/main/java/Interpreter.java
@@ -1,2 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Interpreter {
+    public static List<String> getAlphabetChars(int chars) {
+        List<String> allChars = new ArrayList<>();
+        List<String> alphabetGenerated = new ArrayList<>();
+        try  {
+            allChars = Files.readAllLines(Paths.get(System.getProperty("user.dir")+"/res/RunicCharacters.txt"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        Random random = new Random();
+        for(int i = 0; i < chars; i++) {
+            alphabetGenerated.add(allChars.remove(random.nextInt(allChars.size())));
+        }
+
+        return alphabetGenerated;
+    }
+
+    public static List<String> getAllWords() {
+        List<String> wordList = new ArrayList<>();
+        String basePath = System.getProperty("user.dir") + "/res/";
+
+        try {
+            wordList.addAll(Files.readAllLines(Paths.get(basePath + "Elements.txt")));
+            wordList.addAll(Files.readAllLines(Paths.get(basePath + "Shapes_Evocation.txt")));
+            wordList.addAll(Files.readAllLines(Paths.get(basePath + "Modifiers_Evocation.txt")));
+            wordList.addAll(Files.readAllLines(Paths.get(basePath + "PowerLevels.txt")));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return wordList;
+    }
+
 }
--- src/main/java/MagSysGen.java
+++ src/main/java/MagSysGen.java
@@ -1,2 +1,58 @@
+import java.util.*;
+import java.util.function.Consumer;
+
 public class MagSysGen {
+    public Map<String, String> dictionary;
+    public List<String> alphabet;
+    public int alphabetLength;
+    public int magicWordLength;
+
+    public MagSysGen() {
+        this.alphabetLength = new Random().nextInt(20) + 5;
+        this.alphabet = Interpreter.getAlphabetChars(alphabetLength);
+        this.dictionary = new HashMap<>();
+        this.magicWordLength = new Random().nextInt(5) + 2;
+        this.makeDictionary();
+    }
+
+    private void makeDictionary() {
+        List<String> allWords = Interpreter.getAllWords();
+        int size = allWords.size();
+        Random random = new Random();
+        for(int i = 0; i < size; i++) {
+            // this makes longer alphabets more powerful because they have more spells.
+            // this somewhat balances the endless frustration of trying to find spells.
+            if(random.nextGaussian() * 10 + (this.alphabetLength - 10) < 0) {
+                allWords.remove(random.nextInt(allWords.size()));
+            }
+        }
+
+        for(int i = 0; i < allWords.size(); i++) {
+            StringBuilder magicWord = new StringBuilder();
+            for(int j = 0; j < this.magicWordLength; j++) {
+                magicWord.append(alphabet.get(random.nextInt(alphabet.size())));
+            }
+            // this will result in more words not getting in the language, due to overwriting -- by design
+            this.dictionary.put(magicWord.toString(), allWords.get(i));
+        }
+    }
+
+    public static Consumer<Map.Entry<String, String>> consumer = new EntryConsumer();
+
+    public static void main(String[] args) {
+        MagSysGen sysGen = new MagSysGen();
+        System.out.println(sysGen.alphabetLength);
+        System.out.println(sysGen.alphabet);
+        System.out.println(sysGen.magicWordLength);
+        Iterator<Map.Entry<String, String>> iterator = sysGen.dictionary.entrySet().iterator();
+
+        iterator.forEachRemaining(consumer);
+    }
+
+    private static class EntryConsumer implements Consumer<Map.Entry<String, String>> {
+        @Override
+        public void accept(Map.Entry<String, String> stringStringEntry) {
+            System.out.println(stringStringEntry.getKey() + ": " + stringStringEntry.getValue());
+        }
+    }
 }
